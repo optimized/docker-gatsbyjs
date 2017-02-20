95dae1f9bbb956368e1e2a7aef24fab8c5b25136
Matic Lesjak <matic.lesjak@gmail.com>
2017-02-20 22:31:54 +1300

    Install package dependencies on image startup.
    If this step is not done, you must localy run npm install. This
    would limit image usage on build servers and prevent this image
    working on MacOS since node-sass module would install MacOS
    dependencies and try to run them in Linux container.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+# remove node modules to prevent node-sass platform confilcts
+rm -rf ./node_modules/
+
+# installing node modules to make this image work in build servers
+npm install 
 
 if [ "$1" != "new" ] && ! grep -q "gatsby build" /srv/package.json
 then
